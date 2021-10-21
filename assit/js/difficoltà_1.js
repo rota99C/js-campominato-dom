75f56bf7cd2fd0703307f51ddf0372e104e261ed
rota99C <rotaboolean@gmail.com>
2021-10-21 17:29:43 +0200

    assegnato numeri random bombe in un array

--- assit/js/difficoltà_1.js
+++ assit/js/difficoltà_1.js
@@ -1,8 +1,9 @@
-//L'utente indica un livello di difficoltà
 
 let container = document.querySelector('.container')
 
+// creo la funzione per generare la griglia di ogni livello
 function creaGriglia(numero) {
+    container.innerHTML = ""
     for (let index = 0; index < numero; index++) {
         let radice = Math.sqrt(numero)
         container.style.width = radice * 100 + "px"
@@ -15,33 +16,56 @@ function creaGriglia(numero) {
         elGriglia.appendChild(numGriglia)
         numGriglia.innerHTML = index + 1
 
-
-        elGriglia.addEventListener("click", function () {
+        /* elGriglia.addEventListener("click", function () {
             elGriglia.classList.add("azzurro");
-        });
+        }); */
     }
+}
+
+// creo la funzione per generare le 16 bombe ad ogni livello
+function bombGenerator(numero) {
+    let bombe = []
+    while (bombe.length < 16) {
+        let bomb = Math.floor(Math.random() * numero) + 1;
+        if (!bombe.includes(bomb)) {
+            bombe.push(bomb)
+        }
+    }
+    return bombe
 }
 
 document.querySelector(".btn1").addEventListener("click", function () {
     numQuadrati = 100;
     creaGriglia(numQuadrati)
+    let bombPosition = bombGenerator(numQuadrati)
+    // console.log(bombPosition);
+
 })
 
 document.querySelector(".btn2").addEventListener("click", function () {
     numQuadrati = 81;
     creaGriglia(numQuadrati)
+    let bombPosition = bombGenerator(numQuadrati)
+    // console.log(bombPosition);
 })
 
 
 document.querySelector(".btn3").addEventListener("click", function () {
     numQuadrati = 49;
     creaGriglia(numQuadrati)
+    let bombPosition = bombGenerator(numQuadrati)
+    // console.log(bombPosition);
+})
+
+// creo la funzione per la quale le celle si colorano di azzurro o rosso al click
+
+let cella = document.querySelector(".cella")
+
+cella.addEventListener("click", function () {
+    cella.classList.add("azzurro");
 })
 
 
-
-//dico quante bombe ci sono
-const numerobombe = 16
 
 //collego punteggio
 const punteggio = document.querySelector(".punteggio")
