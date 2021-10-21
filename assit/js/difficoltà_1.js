a9ed94b83e1d122cb332f4c66907db9d47443f07
rota99C <rotaboolean@gmail.com>
2021-10-21 15:47:03 +0200

    griglia con i quadrati cliccando su ogni quadrato diventano azzurri

---
+++ assit/js/difficoltà_1.js
@@ -0,0 +1,43 @@
+//L'utente indica un livello di difficoltà
+
+let container = document.querySelector('.container')
+
+function creaGriglia(numero) {
+    for (let index = 0; index < numero; index++) {
+        let radice = Math.sqrt(numero)
+        container.style.width = radice * 100 + "px"
+        container.style.maxWidth = radice * 100 + "px"
+        const elGriglia = document.createElement('div')
+        elGriglia.className = ('cella')
+        container.appendChild(elGriglia)
+        elGriglia.addEventListener("click", function () {
+            elGriglia.classList.add("azzurro");
+        });
+    }
+}
+
+document.querySelector(".btn1").addEventListener("click", function () {
+    numQuadrati = 100;
+    creaGriglia(numQuadrati)
+})
+
+document.querySelector(".btn2").addEventListener("click", function () {
+    numQuadrati = 81;
+    creaGriglia(numQuadrati)
+})
+
+
+document.querySelector(".btn3").addEventListener("click", function () {
+    numQuadrati = 49;
+    creaGriglia(numQuadrati)
+})
+
+
+
+//dico quante bombe ci sono
+const numerobombe = 16
+
+//collego punteggio
+const punteggio = document.querySelector(.punteggio)
+
+
